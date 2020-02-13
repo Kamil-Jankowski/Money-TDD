3f126c09121539e75d20e5e4a329ac79a68201f0
Kamil <53897136+KamilJ90@users.noreply.github.com>
2020-02-13 21:26:54 +0100

    Initial commit

---
+++ src/main/java/money/Dollar.java
@@ -0,0 +1,14 @@
+package money;
+
+class Dollar {
+
+    public int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
