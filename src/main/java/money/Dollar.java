f74bb8e03ebcbeca41ad18b8033c67c366a2d831
Kamil <53897136+KamilJ90@users.noreply.github.com>
2020-02-14 19:59:04 +0100

    equals() method added

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,5 +1,7 @@
 package money;
 
+import java.util.Objects;
+
 class Dollar {
 
     public int amount;
@@ -11,4 +13,11 @@ class Dollar {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
+
+    @Override
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return this.amount == dollar.amount;
+    }
+
 }
